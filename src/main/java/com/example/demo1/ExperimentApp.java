d171ab3b6a2e3de1b4fed8b3300c428db0da1d6b
adeodato01 <arvin_deodato@hotmail.com>
2022-04-11 20:16:40 -0700

    First Commit

---
+++ src/main/java/com/example/demo1/ExperimentApp.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import java.util.Scanner;
+
+public class ExperimentApp {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Enter the text");
+        String text = scnr.nextLine();
+        System.out.println("input: " + text);
+    }
+}
